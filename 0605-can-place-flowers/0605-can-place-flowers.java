afda31b1149742786374ac0c3b0b24b792cda1de
hms.ideal.v <73270553+1Min-seo@users.noreply.github.com>
2024-10-26 20:43:44 +0900

    Time: 1 ms (95.68%), Space: 45.8 MB (15.13%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int cnt=n;
-    
         for(int i=0; i<flowerbed.length; i++){
             if(cnt==0) return true;
             if(flowerbed[i]==0){
-                boolean emptyLeftPlot= (i==0) || (flowerbed[i-1]==0);
-                boolean emptyRightPlot= (i==flowerbed.length-1) || (flowerbed[i+1]==0);
+                boolean emptyLeftSpot= i==0 || flowerbed[i-1]==0;
+                boolean emptyRightSpot= i==flowerbed.length-1 || flowerbed[i+1]==0;
 
-                if(emptyLeftPlot && emptyRightPlot){
-                flowerbed[i]=1;
-                cnt--;
+                if(emptyLeftSpot && emptyRightSpot){
+                    flowerbed[i]=1;
+                    cnt--;
                 }
             }
         }
+
         return cnt==0;
     }
 }

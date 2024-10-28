a8343c42dc16b28a45a8891ab8ece10250685f56
hms.ideal.v <73270553+1Min-seo@users.noreply.github.com>
2024-10-28 15:07:23 +0900

    Time: 8 ms (45.23%), Space: 42.9 MB (75.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,15 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] arr = s.trim().split("\\s+");
-                     
+        String arr[]=s.trim().split("\\s+");
         List<String> newArr=new ArrayList<>();
 
-        for(String x:arr){
-            newArr.add(x);
+        for(String ns: arr){
+            newArr.add(ns);
         }
 
         Collections.reverse(newArr);
 
-        return String.join(" ", newArr);
+        return String.join(" ",newArr);
     }
 }

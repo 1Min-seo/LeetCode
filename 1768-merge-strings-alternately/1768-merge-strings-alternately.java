0dd61d7f5443efa856a1fc93aea385ae0f4449e4
한송이 <73270553+1Min-seo@users.noreply.github.com>
2024-11-21 12:22:40 +0900

    Time: 1 ms (83.85%), Space: 41.6 MB (61.7%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,16 +1,14 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int w1Size=word1.length();
-        int w2Size=word2.length();
-        StringBuilder sb=new StringBuilder();
+        StringBuilder sb= new StringBuilder();
 
-        for(int i=0; i<Math.max(w1Size, w2Size); i++){
-            if(i<w1Size){
+        for(int i=0; i<Math.max(word1.length(), word2.length()); i++){
+            if(i<word1.length()){
                 sb.append(word1.charAt(i));
             }
-            if(i<w2Size){
+            if(i<word2.length()){{
                 sb.append(word2.charAt(i));
-            }
+            }}
         }
 
         return sb.toString();

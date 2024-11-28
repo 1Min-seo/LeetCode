38bee917ccfb2055c131220a9462cc180285b92b
한송이 <73270553+1Min-seo@users.noreply.github.com>
2024-11-28 18:48:49 +0900

    Time: 676 ms (5.02%), Space: 57 MB (5.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {  
-        int []result=new int[nums.length];
+       int []result=new int[nums.length];
         result[0]=1;
 
-        //left sum
         for(int i=1; i<nums.length; i++){
             result[i]=nums[i-1]*result[i-1];
+            System.out.print(result[i]+", ");
+        }
+        System.out.println();
+        //num   [1,    2, 3,4]
+        //result 1     1  2 6
+        //right 2*3*4 3*4 4 1
+        //result 
+
+        int []right=new int[nums.length];
+        right[nums.length-1]=1;
+        for(int i=nums.length-2; i>=0; i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+        for(int rightValue:right){
+            System.out.print(rightValue+" ");
         }
 
-        //right sum
-        int right=1;
-        for(int i=result.length-1; i>=0; i--){
-            result[i]=right*result[i];
-            right*=nums[i];
+        for(int i=0; i<nums.length; i++){
+            result[i]=result[i]*right[i];
         }
-
         return result;
-        
+
     }
 }

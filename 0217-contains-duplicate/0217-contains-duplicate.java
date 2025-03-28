99e065d3dedba7478020131a0bf9dd223ed8f48a
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-03-28 21:52:35 +0900

    Time: 40 ms (5.14%), Space: 60.4 MB (11.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,21 +1,28 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm=new HashMap<>();
+    
         Arrays.sort(nums);
-        int prev=nums[0];
-
-        for(int i=0; i<nums.length-1; i++){
-           if(prev==nums[i+1]){
-            return true;
-           }
-           prev=nums[i+1];
+        for(int num: nums){
+            hm.put(num, hm.getOrDefault(num, 0)+1);
+            if(hm.get(num)>1) return true;
+            
         }
 
         return false;
+        
     }
-
 }
 
 /**
-   정렬을 하면 1 1 2 3
+    두개 이상 있는게 있으면 true
 
+    1. 정렬을 한다
+    1 1 2 3
+    해시 사용하면 되지 않을깜..?
+    1 2
+    2 1
+    3 1
+
+    을 해서 1이 아니면 바로 트루..
  */

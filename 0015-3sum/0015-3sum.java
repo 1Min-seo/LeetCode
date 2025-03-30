144e2aedf0db37f78c543f513c8d0aa86a8142b5
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-03-30 23:36:37 +0900

    Time: 31 ms (58.5%), Space: 51.5 MB (71.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        int target=0;
-        
-        for(int i=0; i<nums.length-1; i++){
-            target=i;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int start=target+1;
-            int end=nums.length-1;
 
-            while(start<end){
-                int sum=nums[target]+nums[start]+nums[end];
-                if(sum==0){
-                    List<Integer>added=new ArrayList<>();
-                    added.add(nums[target]);
-                    added.add(nums[start]);
-                    added.add(nums[end]);
-                    result.add(added);
+        for(int i = 0; i<nums.length - 2; i++){
+            //시작점 정하기
+            if(i > 0 && nums[i]==nums[i - 1]) continue;
 
-                    while(start<end && nums[start] == nums[start+1]) start++;
-                    while(start<end && nums[end]== nums[end-1]) end--;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-                    start++;
-                    end--;
-                }else if(sum<0){
-                    start++;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }else if (sum < 0){
+                    left++;
                 }else{
-                    end--;
+                    right--;
                 }
             }
         }
         return result;
     }
 }
+/**
+    -1 -1 0 1
+
+ */

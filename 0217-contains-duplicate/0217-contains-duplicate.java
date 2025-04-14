9888b196b53db59cfe5c4f6ac0a1b2d2c5378d25
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-04-14 22:34:08 +0900

    Time: 18 ms (34.75%), Space: 55 MB (96.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> hs=new HashSet<>();
-    
         Arrays.sort(nums);
-        for(int num: nums){
-            if(!hs.add(num)) return true;
+
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]) return true;
         }
 
         return false;
-        
     }
 }
-
 /**
-    두개 이상 있는게 있으면 true
-
-    1. 정렬을 한다
-    1 1 2 3
-    해시 사용하면 되지 않을까
-    중복된 add 연산이 있으면 true 반환
+    적어도 두번 나오면 true
+    정렬해서 앞뒤로 같은게 나오면 true
  */

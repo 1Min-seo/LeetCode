104221a1fbf6e7f9e6e6b9ccf078ca0a8ed6ca04
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-04-17 11:57:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,34 +3,29 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        for(int i = 0; i<nums.length - 2; i++){
-            //시작점 정하기
-            if(i > 0 && nums[i]==nums[i - 1]) continue;
-
+        for(int i = 0; i <= nums.length - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = nums[i];
             int left = i + 1;
             int right = nums.length - 1;
 
             while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-
+                int sum = target + nums[left] + nums[right];
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
+                    result.add(Arrays.asList(target, nums[left], nums[right]));
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                }else if (sum < 0){
-                    left++;
-                }else{
+                }
+                while(left < right && sum > 0){
                     right--;
+                }
+                while(left < right && sum < 0){
+                    left++;
                 }
             }
         }
         return result;
     }
 }
-/**
-    -1 -1 0 1
-
- */

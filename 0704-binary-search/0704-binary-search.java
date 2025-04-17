20accf45df50c04a13055daafac93ab0c2b502bb
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-04-17 23:23:53 +0900

    Time: 0 ms (100%), Space: 45.5 MB (89.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -6,20 +6,20 @@ class Solution {
         if(nums.length == 1){
             if(nums[0] == target) return 0;
             else return -1;
-        } 
-
+        }
         while(left <= right){
-            int mid = left + (right - left) /2;
+            int mid = (left + right) / 2;
+
+            if(left <= right && nums[mid] < target){
+                left = mid + 1;
+            }
+            else if(left <= right && nums[mid] > target){
+                right = mid - 1;
+            }
             if(nums[mid] == target){
                 return mid;
             }
-            
-            if(nums[mid] <= target){
-                left = mid + 1;
-            }else if(nums[mid] > target){
-                right = mid - 1;
-            }
         }
-        return -1;        
+        return -1;
     }
 }

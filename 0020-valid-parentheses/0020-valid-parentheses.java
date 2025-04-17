268e56afde4ce6267b9c1cefaac999d6a460e0e8
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-04-17 13:12:03 +0900

    Time: 2 ms (97.44%), Space: 41.8 MB (47.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,42 +1,19 @@
-import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        
-        st.push(s.charAt(0));
-        for(int i=1; i<s.length(); i++){
-            if(!st.isEmpty()){
-                char c=st.peek();
-                if(c == '(' &&  s.charAt(i) ==')'){
-                    st.pop();
+        Stack<Character> st = new Stack<>();
 
-                }else if(c == '{' && s.charAt(i) == '}'){
-                    st.pop();
-              
-                }else if(c == '[' && s.charAt(i) == ']'){
-                    st.pop();
-             
-                }else{
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c=='['){
+                st.push(c);
+            }else{
+                if(st.isEmpty()) return false;
 
-                    st.push(s.charAt(i));
-                }
+                char top = st.pop();
+                if(c == ')' && top != '(') return false;
+                else if(c == '}' && top != '{') return false;
+                else if(c == ']' && top != '[') return false;
             }
-            else st.push(s.charAt(i));
-
         }
-
-        for(char num : st) {
-            System.out.print(num+" ");
-        }
-        if(!st.isEmpty()) return false;
-        else return true;
+        return st.isEmpty();
     }
 }
-/**
-    i=0 0 -> (
-    i=1 ) -> st.pop(). => st    empty
-    i=2 { -> st {
-    i=3 } -> st.pop()  => st    empty
-    i=4 } -> st }
-    i=5 { -> st } {
- */

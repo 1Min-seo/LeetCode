9c4c71d1813c7f25ed35a54f6b7561f888c01b08
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-04-17 17:08:45 +0900

    Time: 5 ms (37.29%), Space: 44.6 MB (91.99%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,29 +1,32 @@
 class MinStack {
-    private List<int[]> st;
+    private Stack<Integer> st;
+    private Stack<Integer> minSt;
 
     public MinStack() {
-        st = new ArrayList<>();
+        st = new Stack<>();
+        minSt = new Stack<>();
     }
     
     public void push(int val) {
-        int[] top = st.isEmpty() ? new int[] {val, val} : st.get(st.size() - 1);
-        int min_val = top[1];
-        if(min_val > val){
-            min_val = val;
-        }
-        st.add(new int[]{val, min_val});
+       st.push(val);
+       if(minSt.isEmpty() || val <= minSt.peek()){
+            minSt.push(val);
+       }
     }
     
     public void pop() {
-        st.remove(st.size() - 1);
+        int val = st.pop();
+        if(val == minSt.peek()){
+            minSt.pop();
+        }
     }
     
     public int top() {
-        return st.isEmpty() ? -1 : st.get(st.size() - 1)[0];
+        return st.peek();
     }
     
     public int getMin() {
-        return st.isEmpty() ? -1 : st.get(st.size() - 1 )[1];
+        return minSt.peek();
     }
 }
 

ead7820076349638490f6d65df9d2685dcf8e8ec
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-05-07 09:38:12 +0900

    Time: 0 ms (100%), Space: 42.2 MB (64.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,34 +1,34 @@
 class Solution {
     List<String> result = new ArrayList<>();
-
     public List<String> letterCombinations(String digits) {
         if(digits == null || digits.length() == 0) return result;
 
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
-        
-        dfs(map, digits, 0, new StringBuilder());
+        Map<Character, String> hm = new HashMap<>();
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+
+        dfs(hm, digits, 0, new StringBuilder());
+
         return result;
     }
 
-    private void dfs(Map<Character, String> map, String digits, int idx, StringBuilder path){
+    private void dfs(Map<Character, String> hm, String digits, int idx, StringBuilder sb){
         if(idx == digits.length()) {
-            result.add(path.toString());
+            result.add(sb.toString());
             return;
         }
 
-        String letters = map.get(digits.charAt(idx));
+        String letters = hm.get(digits.charAt(idx));
         for(int i = 0; i < letters.length(); i++) {
-            path.append(letters.charAt(i));
-            dfs(map, digits, idx + 1, path);
-            path.deleteCharAt(path.length() - 1);
+            sb.append(letters.charAt(i));
+            dfs(hm, digits, idx + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
-    }
+    }    
 }

341b761c1483d63e99d70b23f91a80f2148263df
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-06-03 01:10:19 +0900

    Time: 33 ms (37.11%), Space: 52.8 MB (14.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
+        
+        for(int fixed = 0; fixed < nums.length - 2; fixed++) {
+            if(fixed > 0 && nums[fixed] == nums[fixed - 1]) continue;
 
-        for(int i = 0; i <= nums.length - 3; i++){
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int target = nums[i];
-            int left = i + 1;
+            int left = fixed + 1;
             int right = nums.length - 1;
 
-            while(left < right){
-                int sum = target + nums[left] + nums[right];
-                if(sum == 0){
-                    result.add(Arrays.asList(target, nums[left], nums[right]));
+            while(left < right) {
+                int sum = nums[fixed] + nums[left] + nums[right];
+
+                if(sum == 0) {
+                    List<Integer> result = new ArrayList<>();
+                    result.add(nums[fixed]);
+                    result.add(nums[left]);
+                    result.add(nums[right]);
+
+                    answer.add(result);
+
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     while(left < right && nums[right] == nums[right - 1]) right--;
+
                     left++;
                     right--;
-                }
-                while(left < right && sum > 0){
+
+                }else if(sum < 0) {
+                   left++;
+                }else {
                     right--;
                 }
-                while(left < right && sum < 0){
-                    left++;
-                }
-            }
+            }      
         }
-        return result;
+        return answer;
     }
 }
+/**
+    -4 -1 -1 0 1 2
+ */

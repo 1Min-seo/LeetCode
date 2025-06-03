5c4cf2ad3f22f3e4a88ba7b9a7f62671e02e1726
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-06-03 15:18:19 +0900

    Time: 5 ms (75.31%), Space: 58.6 MB (7.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,22 +2,20 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int result = 0;
 
-        while(left < right){
-            int container = (right - left) * (Math.min(height[left], height[right]));
-            result = Math.max(result, container);
-            if(height[left] < height[right]){
+        int amount= 0;
+        int result = 0;
+        while(left < right) {
+            amount = (right - left) * Math.min(height[left], height[right]);
+
+            if(left < right && height[left] < height[right]) {
                 left++;
             }else{
                 right--;
             }
-        }
 
+            result = Math.max(result, amount);
+        }
         return result;
     }
 }
-/**
-    정렬X
-    높이를 비교해서 작으면 작은 쪽에서 옮기기
- */

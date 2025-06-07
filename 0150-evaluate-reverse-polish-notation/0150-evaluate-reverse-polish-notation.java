c0e9814cdcd4cdd285e1f1064b2db8693a8e8169
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-06-07 21:02:59 +0900

    Time: 6 ms (72.16%), Space: 44.6 MB (81.38%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,43 +1,40 @@
-import java.util.*;
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st= new Stack<>();
-        int tmp = 0;
+        Stack<Integer> st = new Stack<>();
+        int result = 0;
+        for(String token : tokens){
+            if(token.equals("+")){
+                int b = st.pop();
+                int a = st.pop();
 
-        for(String str : tokens){
-            switch(str){
-                case "+":
-                 int b = st.pop(); 
-                 int a = st.pop();
-                 tmp = a + b; 
-                 st.push(tmp);
-                 break;
-                case "-":
-                 b = st.pop(); 
-                 a = st.pop();
-                 tmp = a - b; 
-                 st.push(tmp);
-                 break;
-                case "*":
-                 b = st.pop(); 
-                 a = st.pop();
-                 tmp = a * b; 
-                 st.push(tmp);
-                 break;
-                case "/":
-                 b = st.pop(); 
-                 a = st.pop();
-                 if (b == 0) {
-                        throw new ArithmeticException("Cannot divide by zero");
-                    }
-                 tmp = a / b; 
-                 st.push(tmp);
-                 break;
-                default:
-                st.push(Integer.parseInt(str));
+                result = a + b;
+                st.push(result);
+            }
+            else if(token.equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+
+                result = a - b;
+                st.push(result);
+            }
+            else if(token.equals("*")){
+                int b = st.pop();
+                int a = st.pop();
+
+                result = a * b;
+                st.push(result);
+            }
+            else if(token.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+
+                result = a / b;
+                st.push(result);
+            }else{
+                st.push(Integer.parseInt(token));
             }
         }
 
-        return st.pop();
+        return st.peek();
     }
 }

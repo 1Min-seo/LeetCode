2aa03bbb87d51e2de30e2c867099f763b07cdb3a
한송이 <73270553+1Min-seo@users.noreply.github.com>
2025-07-21 23:12:36 +0900

    Time: 159 ms (34.72%), Space: 41.7 MB (47.19%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,16 +3,17 @@ class Solution {
     private int[] dy = {0, -1, 1, 0};
     private int m, n;
     private boolean[][] visited;
-    private boolean found = false;
+    boolean found = false;
 
     public boolean exist(char[][] board, String word) {
         m = board.length;
         n = board[0].length;
         visited = new boolean[m][n];
+       
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == word.charAt(0)){
+                if(board[i][j] == word.charAt(0)) {
                     dfs(board, visited, i, j, 0, word);
                     if(found) return true;
                 }
@@ -21,26 +22,22 @@ class Solution {
         return false;
     }
 
-    public void dfs(char[][] board, boolean[][] visited, int x, int y, int idx, String word) {
-        if(found) return;
-
-        if(idx == word.length() - 1 && board[x][y] == word.charAt(idx)) {
+    private void dfs(char[][] board, boolean[][] visted, int x, int y, int index, String word) {
+        if(board[x][y] == word.charAt(index) && index == word.length() - 1) {
             found = true;
             return;
         }
-
-        visited[x][y] = true;
+         visited[x][y] = true;
 
         for(int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(nx >=0 && ny >=0 && nx < m && ny < n && !visited[nx][ny] 
-            && board[nx][ny] == word.charAt(idx + 1)) {
-                dfs(board, visited, nx, ny, idx + 1, word);
+            if(nx >= 0 && ny >= 0 && nx < m && ny < n 
+            && !visited[nx][ny] && board[nx][ny] == word.charAt(index + 1)) {
+                dfs(board, visited, nx, ny, index + 1, word);
             }
         }
-
         visited[x][y] = false;
     }
 }
